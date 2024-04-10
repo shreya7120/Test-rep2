8312304e61cbfca7b91bf1b37c777948bc31ee2e
user <user@DESKTOP-TR4LISM>
2024-04-10 23:37:59 +0530

    first commit of core java

---
+++ src/if_else/Vote1.java
@@ -0,0 +1,23 @@
+package if_else;
+
+public class Vote1 {
+
+
+	public static void main(String[] args) {
+		
+		int age=15;
+		
+		if(age>=18)
+		{
+			System.out.println("Eligible...");	
+		}
+		else
+		{
+			System.out.println("Not Eligible...");
+		}
+
+
+		
+	}
+
+}
